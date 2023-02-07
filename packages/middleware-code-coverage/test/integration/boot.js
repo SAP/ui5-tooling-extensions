6a65d4b470fa98dd5203df460e73fed9c9c3623f
Florian Vogt <florian.vogt@sap.com>
2023-02-07 14:49:28 +0100

    ci(test): enhance test matrix to cover supported os and node versions (#14)

--- packages/middleware-code-coverage/test/integration/boot.js
+++ packages/middleware-code-coverage/test/integration/boot.js
@@ -1,53 +1,83 @@
 import test from "ava";
 import path from "node:path";
 import getPort from "get-port";
-import waitOn from "wait-on";
 import request from "supertest";
 import {spawn} from "node:child_process";
 
 const baseConfigPath = "test/integration/fixtures/config/";
 
+let install;
+
+function exec(command, args=[]) {
+	return spawn(command, args, {
+		shell: true,
+		cwd: "test/integration/fixtures/ui5-app/"
+	});
+}
+
+function setup() {
+	if (!install) {
+		const child = exec("npm i --install-links=false");
+
+		install = new Promise( (resolve) => {
+			child.on("close", resolve);
+		});
+	}
+	return install;
+}
+
+function startUI5Server(configPath, port) {
+	const child = exec(`npm start`, [
+		`-- --config ${configPath} --port ${port}`,
+	]);
+
+	return new Promise( (resolve, reject) => {
+		const onError = () => {
+			reject(new Error("Start of UI5 Server failed."));
+		};
+		const onSuccess = (data) => {
+			data = data ? data.toString() : "";
+			if (data.startsWith("URL: http://localhost:")) {
+				resolve();
+			} else if (data.startsWith("Process Failed With Error")) {
+				onError();
+			}
+		};
+
+		child.stdout.on("data", onSuccess);
+		child.on("close", onError);
+	});
+}
+
 async function startUI5App(config = "./ui5.yaml") {
 	const configPath = path.resolve(config);
 	const port = await getPort();
-	// start ui5-app
-	const childProcess = spawn(`npm i && npm start`, [
-		`-- --config ${configPath} --port ${port}`,
-	], {
-		// stdio: "inherit", // > don't include stdout in test output,
-		shell: true,
-		cwd: "test/integration/fixtures/ui5-app/",
-		detached: true, // this for being able to kill all subprocesses of above `ui5 serve` later
-	});
 
-	await waitOn({resources: [`http://localhost:${port}`]});
+	await startUI5Server(configPath, port);
+
 	const app = request(`http://localhost:${port}`);
-	return {childProcess, app};
+	return {app};
 }
 
-function shutDownUI5App(childProcess) {
-	// kill all processes that are in the same pid group (see detached: true)
-	process.kill(-childProcess.pid);
-}
+test.before(async (t) => {
+	t.timeout(500000);
+	await setup();
+});
 
 test.serial("Ping endpoint is up and running", async (t) => {
-	const {childProcess, app} = await startUI5App(baseConfigPath + "ui5-simple.yaml");
+	const {app} = await startUI5App(baseConfigPath + "ui5-simple.yaml");
 
 	const pingResponse = await app.get("/.ui5/coverage/ping");
 	t.is(pingResponse.status, 200, "Ping was successful");
 	t.is(pingResponse.type, "application/json", "Content-Type is correct");
 	t.truthy(pingResponse.body.version, "Version is valid");
-
-	shutDownUI5App(childProcess);
 });
 
 test.serial("Send coverage report", async (t) => {
-	const {childProcess, app} = await startUI5App(baseConfigPath + "ui5-simple.yaml");
+	const {app} = await startUI5App(baseConfigPath + "ui5-simple.yaml");
 
 	// TODO: send valid content to the report endpoint and check result
 	const reportResponse = await app.post("/.ui5/coverage/report");
 
 	t.is(reportResponse.type, "application/json", "Content-Type is correct");
-
-	shutDownUI5App(childProcess);
 });
