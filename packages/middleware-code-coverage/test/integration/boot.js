5618ab7bf97b36116d10f7b9314c4ffea08be0db
Yavor Ivanov <yavor.ivanov@sap.com>
2023-09-08 15:51:25 +0300

    test(middleware-code-coverage): add more integration tests
    Add integration tests, so that all of the end points could be covered

--- packages/middleware-code-coverage/test/integration/boot.js
+++ packages/middleware-code-coverage/test/integration/boot.js
@@ -9,6 +9,59 @@ const TEST_TIMEOUT = 5 * 60 * 1000; // 5 minutes
 
 let install;
 
+const globalCoverageMap = {
+	"/resources/Control1.js": {
+		"path": "/resources/Control1.js",
+		"statementMap": {
+			"0": {
+				"start": {
+					"line": 7,
+					"column": 0
+				},
+				"end": {
+					"line": 31,
+					"column": 3
+				}
+			},
+			"1": {
+				"start": {
+					"line": 11,
+					"column": 17
+				},
+				"end": {
+					"line": 11,
+					"column": 26
+				}
+			},
+			"2": {
+				"start": {
+					"line": 17,
+					"column": 18
+				},
+				"end": {
+					"line": 28,
+					"column": 2
+				}
+			},
+			"3": {
+				"start": {
+					"line": 19,
+					"column": 3
+				},
+				"end": {
+					"line": 21,
+					"column": 4
+				}
+			}
+		},
+		"fnMap": {},
+		"branchMap": {},
+		"s": {},
+		"f": {},
+		"b": {}
+	}
+};
+
 function exec(command, args=[]) {
 	return execa(command, args, {
 		shell: true,
@@ -90,8 +143,25 @@ test.serial("Ping endpoint is up and running", async (t) => {
 test.serial("Send coverage report", async (t) => {
 	const {app} = t.context;
 
-	// TODO: send valid content to the report endpoint and check result
-	const reportResponse = await app.post("/.ui5/coverage/report");
-
+	const reportResponse = await app.post("/.ui5/coverage/report", globalCoverageMap);
 	t.is(reportResponse.type, "application/json", "Content-Type is correct");
+
+	const responseBody = JSON.parse(reportResponse.res.text);
+
+	t.true(Array.isArray(responseBody.coverageMap), "coverageMap is present and is an array");
+	t.true(Array.isArray(responseBody.availableReports), "availableReports is present and is an array");
+	t.true(responseBody.availableReports.some((report) => report.report === "html"), "Has HTML report by default");
+});
+
+test.serial("Coverage report is generated and available", async (t) => {
+	const {app} = t.context;
+
+	await app.post("/.ui5/coverage/report", globalCoverageMap);
+
+	const htmlReportResponse = await app.get("/.ui5/coverage/report/html/index.html");
+	t.is(htmlReportResponse.status, 200, "HTML Report is generated and available");
+	t.truthy(
+		htmlReportResponse.text.includes("<title>Code coverage report"),
+		"HTML Report is not a real coverage page"
+	);
 });
