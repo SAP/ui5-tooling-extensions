abf012cb4668c3632d77afdb5830afab915e7eac
Yavor Ivanov <d3xter666@users.noreply.github.com>
2023-09-14 12:49:29 +0300

    test(middleware-code-coverage): timeout hanging integration tests (#151)
    JIRA: CPOUI5FOUNDATION-727
    
    The integration tests rely on some specific input or an exception from
    the console in order to bootstrap.
    Missing the specific data causes hanging in the tests and there's no
    mechanism to timeout and break the tests.
    
    Now, tests rely on `execa` module that is used also in the other ui5
    tooling packages and there's a timeout to break the tests.
    
    Some more verbose logging is added to the bootstrapping, so debugging
    could be easier in the future.
    
    More tests were added

--- packages/middleware-code-coverage/test/integration/boot.js
+++ packages/middleware-code-coverage/test/integration/boot.js
@@ -1,45 +1,89 @@
 import test from "ava";
 import path from "node:path";
+import {fileURLToPath} from "node:url";
 import getPort from "get-port";
 import request from "supertest";
-import {spawn} from "node:child_process";
+import {execa} from "execa";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const TEST_TIMEOUT = 5 * 60 * 1000; // 5 minutes
 
-const baseConfigPath = "test/integration/fixtures/config/";
-
-let install;
+const globalCoverageMap = {
+	"/resources/Control1.js": {
+		"path": "/resources/Control1.js",
+		"statementMap": {
+			"0": {
+				"start": {
+					"line": 7,
+					"column": 0
+				},
+				"end": {
+					"line": 31,
+					"column": 3
+				}
+			},
+			"1": {
+				"start": {
+					"line": 11,
+					"column": 17
+				},
+				"end": {
+					"line": 11,
+					"column": 26
+				}
+			},
+			"2": {
+				"start": {
+					"line": 17,
+					"column": 18
+				},
+				"end": {
+					"line": 28,
+					"column": 2
+				}
+			},
+			"3": {
+				"start": {
+					"line": 19,
+					"column": 3
+				},
+				"end": {
+					"line": 21,
+					"column": 4
+				}
+			}
+		},
+		"fnMap": {},
+		"branchMap": {},
+		"s": {},
+		"f": {},
+		"b": {}
+	}
+};
 
 function exec(command, args=[]) {
-	return spawn(command, args, {
-		shell: true,
-		cwd: "test/integration/fixtures/ui5-app/"
-	});
-}
-
-function setup() {
-	if (!install) {
-		const child = exec("npm i --install-links=false");
-
-		install = new Promise( (resolve) => {
-			child.on("close", resolve);
-		});
-	}
-	return install;
+	return execa(command, args, {
+		cwd: path.join(__dirname, "fixtures", "ui5-app"),
+		preferLocal: true
+	})
+		.pipeStdout(process.stdout)
+		.pipeStderr(process.stderr);
 }
 
 function startUI5Server(configPath, port) {
-	const child = exec(`npm start`, [
-		`-- --config ${configPath} --port ${port}`,
-	]);
+	// Starting the app this way allows AVA to directly manage and kill subprocesses like "ui5 serve".
+	// Using App's 'npm start' script would start a (detached) subprocess and that
+	// would require more efforts to find and kill it.
+	const child = exec("ui5", ["serve", "--config", configPath, "--port", port]);
 
 	return new Promise( (resolve, reject) => {
-		const onError = () => {
-			reject(new Error("Start of UI5 Server failed."));
+		const onError = (errMessage = "Start of UI5 Server failed.") => {
+			reject(new Error(errMessage));
 		};
 		const onSuccess = (data) => {
 			data = data ? data.toString() : "";
-			if (data.startsWith("URL: http://localhost:")) {
+			if (data.includes("URL: http://localhost:")) {
 				resolve();
-			} else if (data.startsWith("Process Failed With Error")) {
+			} else if (data.includes("Process Failed With Error")) {
 				onError();
 			}
 		};
@@ -60,12 +104,14 @@ async function startUI5App(config = "./ui5.yaml") {
 }
 
 test.before(async (t) => {
-	t.timeout(500000);
-	await setup();
+	t.timeout(TEST_TIMEOUT);
+
+	const {app} = await startUI5App(path.join(__dirname, "fixtures", "config", "ui5-simple.yaml"));
+	t.context.app = app;
 });
 
 test.serial("Ping endpoint is up and running", async (t) => {
-	const {app} = await startUI5App(baseConfigPath + "ui5-simple.yaml");
+	const {app} = t.context;
 
 	const pingResponse = await app.get("/.ui5/coverage/ping");
 	t.is(pingResponse.status, 200, "Ping was successful");
@@ -74,10 +120,40 @@ test.serial("Ping endpoint is up and running", async (t) => {
 });
 
 test.serial("Send coverage report", async (t) => {
-	const {app} = await startUI5App(baseConfigPath + "ui5-simple.yaml");
+	const {app} = t.context;
 
-	// TODO: send valid content to the report endpoint and check result
-	const reportResponse = await app.post("/.ui5/coverage/report");
-
+	const reportResponse = await app.post("/.ui5/coverage/report", globalCoverageMap);
 	t.is(reportResponse.type, "application/json", "Content-Type is correct");
+
+	const responseBody = JSON.parse(reportResponse.res.text);
+
+	t.true(Array.isArray(responseBody.coverageMap), "coverageMap is present and is an array");
+	t.true(Array.isArray(responseBody.availableReports), "availableReports is present and is an array");
+	t.true(responseBody.availableReports.some((report) => report.report === "html"), "Has HTML report by default");
+});
+
+test.serial("Coverage report is generated and available", async (t) => {
+	const {app} = t.context;
+
+	await app.post("/.ui5/coverage/report", globalCoverageMap);
+
+	const htmlReportResponse = await app.get("/.ui5/coverage/report/html/index.html");
+	t.is(htmlReportResponse.status, 200, "HTML Report is generated and available");
+	t.truthy(
+		htmlReportResponse.text.includes("<title>Code coverage report"),
+		"HTML Report is a real coverage page"
+	);
+});
+
+test.serial("Coverage report is unavailable", async (t) => {
+	const {app} = t.context;
+
+	await app.post("/.ui5/coverage/report", globalCoverageMap);
+
+	const nonExistingReportReportResponse = await app.get("/.ui5/coverage/report/cobertura");
+	t.is(nonExistingReportReportResponse.status, 404, "cobertura report has not been generated");
+	t.falsy(
+		nonExistingReportReportResponse.text.includes("<title>Code coverage report"),
+		"A non coverage report page is returned"
+	);
 });
